b99151c588492041ae6b2b76d1aeb0894dbc9180
zhugaopeng <zhugaopeng@zhugaopengdeMacBook-Air.local>
2015-11-01 12:47:23 +0800

    init

---
+++ TangEmblem-library/TangEmblem_library.h
@@ -0,0 +1,13 @@
+//
+//  TangEmblem_library.h
+//  TangEmblem-library
+//
+//  Created by zhugaopeng on 15/11/1.
+//  Copyright (c) 2015年 tanghulu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TangEmblem_library : NSObject
+
+@end
---
+++ TangEmblem-library/TangEmblem_library.m
@@ -0,0 +1,13 @@
+//
+//  TangEmblem_library.m
+//  TangEmblem-library
+//
+//  Created by zhugaopeng on 15/11/1.
+//  Copyright (c) 2015年 tanghulu. All rights reserved.
+//
+
+#import "TangEmblem_library.h"
+
+@implementation TangEmblem_library
+
+@end
