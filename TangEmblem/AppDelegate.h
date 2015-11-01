1fc2d519baf3cc72eaa5938ba7995668a5f41f34
zhugaopeng <zhugaopeng@zhugaopengdeMacBook-Air.local>
2015-11-01 12:24:16 +0800

    Initial Commit

---
+++ TangEmblem/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  TangEmblem
+//
+//  Created by zhugaopeng on 15/11/1.
+//  Copyright tanghulu 2015年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ TangEmblem/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  TangEmblem
+//
+//  Created by zhugaopeng on 15/11/1.
+//  Copyright tanghulu 2015年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
